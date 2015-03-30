a2ae3821f7cbf2c30561fe2b444a064fcdbac598
Georg Lochmann <georg.lochmann@gmail.com>
2015-03-30 17:45:58 +0200

    [UPD] geoIP

--- src/Iam/IPBundle/Controller/DefaultController.php
+++ src/Iam/IPBundle/Controller/DefaultController.php
@@ -11,7 +11,32 @@ class DefaultController extends Controller
      * @Route("/", name="_ip")
      */
     public function indexAction() {
-        $ip = "000";
-        return $this->render('IamIPBundle:Default:index.html.twig', array('ip_address' => $ip));
+        $ipAddress = '131.130.58.211'; //wien
+//        $ipAddress = '129.27.2.3'; //graz
+//        $ipAddress = '141.84.147.198'; //garching
+//        $ipAddress = '138.232.1.1'; //innsbruck
+//        $ipAddress = '131.159.42.0'; //münchen
+//        $ipAddress = '81.169.145.154'; //berlin
+        $geoip = $this->get('maxmind.geoip')->lookup($ipAddress);
+
+        return $this->render('IamIPBundle:Default:index.html.twig', array(
+            'ipAddress' => $ipAddress,
+            'geoip' => $geoip
+        ));
+    }
+    
+    private function getUserIP() {
+        if( array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+            if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',') > 0) {
+                $addr = explode(",",$_SERVER['HTTP_X_FORWARDED_FOR']);
+                return trim($addr[0]);
+            }
+            else {
+                return $_SERVER['HTTP_X_FORWARDED_FOR'];
+            }
+        }
+        else {
+            return $_SERVER['REMOTE_ADDR'];
+        }
     }
 }
